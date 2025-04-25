144f8fec04d405ca4086a3dd45ad6f0d5be7c4b8
MariaRaba <Maria.Raba@tptlive.ee>
2025-04-25 21:15:59 +0300

    mmm table

--- views/posts/view.php
+++ views/posts/view.php
@@ -1,33 +1,43 @@
 <?php include __DIR__ . '/../partials/header.php'  ?>
 
 <main class="container mt-4">
-        <h2>Post Details</h2>
-        <br>
-        <div class="mb-3">
-            <strong>ID:</strong> <?= $post->id; ?>
-        </div>
+    <h2>Post Details</h2>
+    <br>
 
-        <div class="mb-3">
-            <strong>Title:</strong> <?= $post->title; ?>
-        </div>
-        
-        <div class="mb-3">
-            <strong>Content:</strong> <?= $post->body; ?>
-        </div>
-        
-        <div class="mb-3">
-            <strong>Created at:</strong> <?= $post->created_at; ?>
-        </div>
+    <table class="table table-bordered">
+        <tr>
+            <th>ID:</th>
+            <td><?= $post->id; ?></td>
+        </tr>
+        <tr>
+            <th>Title:</th>
+            <td><?= $post->title; ?></td>
+        </tr>
+        <tr>
+            <th>Content:</th>
+            <td><?= $post->body; ?></td>
+        </tr>
+        <tr>
+            <th>Created at:</th>
+            <td><?= $post->created_at; ?></td>
+        </tr>
+        <tr>
+            <th>Published at:</th>
+            <td><?= $post->updated_at; ?></td>
+        </tr>
+    </table>
 
-        <div class="mb-3">
-            <strong>Published at:</strong> <?= $post->updated_at; ?>
-        </div>
-
-        <div class="mb-3">
-            <a href="/admin/posts" class="btn btn-secondary">Back to Posts</a>
-        </div>
-    </article>
+    <div class="mb-3 mt-3">
+        <a href="/admin/posts" class="btn btn-secondary">Back to Posts</a>
+    </div>
 </main>
 
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding: 8px;
+}
+</style>
 
 <?php include __DIR__ . '/../partials/footer.php'  ?>
